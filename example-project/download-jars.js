c55a5d08dbbcf70d4138898dbea4437d1cea824a
Eirik <eirikmyklebost+github@gmail.com>
2021-10-12 17:13:54 +0200

    upgraded to package-lock v2

--- example-project/download-jars.js
+++ example-project/download-jars.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const download = require('download');
- 
+
 const downloads = [
     'https://github.com/hapifhir/org.hl7.fhir.core/releases/latest/download/validator_cli.jar',
     'https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.8.1/junit-platform-console-standalone-1.8.1.jar',
